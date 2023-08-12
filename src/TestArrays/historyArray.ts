e68534b1fc9b6a28dde23619abf8276b4eaf0559
Daniil Grebnev <danilgrebneff@yandex.ru>
2023-08-12 19:48:28 +0300

    fix gitignore

---
+++ src/TestArrays/historyArray.ts
@@ -0,0 +1,44 @@
+export const historyItems: IHistory[] = [
+	{
+		id: '1',
+		title: 'Создание института!',
+		image: <IImages[]>[
+			{
+				img: '',
+			},
+			{
+				img: '',
+			},
+		],
+	},
+	{
+		id: '1',
+		title: 'Создание института!',
+		image: <IImages[]>[
+			{
+				img: '',
+			},
+		],
+	},
+	{
+		id: '1',
+		title: 'Создание института!',
+		image: <IImages[]>[
+			{
+				img: '',
+			},
+			{
+				img: '',
+			},
+		],
+	},
+	{
+		id: '1',
+		title: 'Создание института!',
+		image: <IImages[]>[
+			{
+				img: '',
+			},
+		],
+	},
+]
