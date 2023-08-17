229fc98f5588842e6d96554b4e1dfe0ba4a0de24
Daniil Grebnev <danilgrebneff@yandex.ru>
2023-08-17 23:23:06 +0300

    about bg исправлено и добавлена корова

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,26 @@
+import cow from '@/cow.png'
+import Image from 'next/image'
 import Link from 'next/link'
 
 const NotFound = () => {
 	return (
-		<div className='h-screen w-full flex items-center justify-center bg-black text-white'>
-			404 - Not Found d
-			<div
-				className='
-			'
-			>
-				<Link href='/'>Main</Link>
+		<div className='bg-slate-100 min-h-screen flex items-center justify-center'>
+			<div className=''>
+				<Image src={cow} alt='Cow' width={400} />
+			</div>
+			<div className='ml-32'>
+				<h1 className='text-[#0F91D6] text-[150px] text-center font-bold'>
+					404
+				</h1>
+				<p className='text-[#0F91D6] text-center  text-3xl mb-20'>
+					Страница не найдена
+				</p>
+				<Link
+					href={`/`}
+					className='rounded w-[400px] text-white mx-auto bg-[#0f91d6] px-36 text-lg py-5'
+				>
+					На главную
+				</Link>
 			</div>
 		</div>
 	)
