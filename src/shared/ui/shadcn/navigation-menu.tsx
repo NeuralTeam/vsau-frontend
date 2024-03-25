18225b3fb337581ab807256d1c2d9b9908853e74
Osborn <dencat4785@gmail.com>
2024-03-25 16:05:52 +0300

    feat: изменены цвета панели навигации

--- src/shared/ui/shadcn/navigation-menu.tsx
+++ src/shared/ui/shadcn/navigation-menu.tsx
@@ -31,7 +31,7 @@ NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName;
 const NavigationMenuItem = NavigationMenuPrimitive.Item;
 
 const navigationMenuTriggerStyle = cva(
-    "group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
+    "group inline-flex h-10 w-max items-center justify-center rounded-md bg-[#0F91D6] px-4 py-2 text-sm font-medium transition-colors hover:bg-accent/30 disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/30 data-[state=open]:bg-accent/30"
 );
 
 const NavigationMenuTrigger = React.forwardRef<
