029a15a91ef2c05da50e1d4656b7adcbcc57fa8a
Osborn <dencat4785@gmail.com>
2024-07-07 19:12:24 +0300

    feat: добавлены страница "документы"

--- src/app/sveden/document/page.tsx
+++ src/app/sveden/document/page.tsx
@@ -11,36 +11,56 @@ const SvedenDocumentPage = () => {
                 <ul className="space-y-2">
                     <li className="flex items-center text-[18px] text-[#0F91D6] underline underline-offset-2">
                         <Dot className="min-h-6 min-w-6" />
-                        <Link href="#">Устав ФГБОУ ВО Воронежский ГАУ</Link>
+                        <Link href="#" itemProp="ustavDocLink">
+                            Устав ФГБОУ ВО Воронежский ГАУ
+                        </Link>
                     </li>
                 </ul>
             </div>
 
             <div className="space-y-2 rounded-[10px] bg-white p-8">
                 <p className="text-[23px] font-semibold leading-[20px]">
-                    Информация о лицензии на осуществлении образовательной деятельности (с приложениями)
+                    Локальных нормативных актов, предусмотренных частью 2 статьи 30 настоящего федерального закона, правил внутреннего распорядка
+                    обучающихся, правил внутреннего трудового распорядка, коллективного договора
                 </p>
                 <ul className="space-y-2">
                     <li className="flex items-center text-[18px] text-[#0F91D6] underline underline-offset-2">
                         <Dot className="min-h-6 min-w-6" />
-                        <Link href="#">Выписка из реестра лицензий на осуществление образовательной деятельности от 22.01.2024</Link>
+                        <Link href="#" itemProp="localActStud">
+                            Правила внутреннего распорядка обучающихся
+                        </Link>
+                    </li>
+                    <li className="flex items-center text-[18px] text-[#0F91D6] underline underline-offset-2">
+                        <Dot className="min-h-6 min-w-6" />
+                        <Link href="#" itemProp="localActOrder">
+                            Правила внутреннего трудового распорядка
+                        </Link>
+                    </li>
+                    <li className="flex items-center text-[18px] text-[#0F91D6] underline underline-offset-2">
+                        <Dot className="min-h-6 min-w-6" />
+                        <Link href="#" itemProp="localActCollec">
+                            Коллективный договор
+                        </Link>
                     </li>
                 </ul>
             </div>
 
             <div className="space-y-2 rounded-[10px] bg-white p-8">
-                <p className="text-[23px] font-semibold leading-[20px]">Свидетельство о государственной аккредитации (с приложениями)</p>
+                <p className="text-[23px] font-semibold leading-[20px]">Отчет о результатах самообследования</p>
                 <ul className="space-y-2">
                     <li className="flex items-center text-[18px] text-[#0F91D6] underline underline-offset-2">
                         <Dot className="min-h-6 min-w-6" />
-                        <Link href="#">Свидетельство о государственной аккредитации</Link>
+                        <Link href="#" itemProp="reportEduDocLink">
+                            Отчёт о результатах самообследования
+                        </Link>
                     </li>
                     <li className="flex items-center text-[18px] text-[#0F91D6] underline underline-offset-2">
                         <Dot className="min-h-6 min-w-6" />
-                        <Link href="#">
-                            Выписка из государственной информационной системы «Реестр организаций, осуществляющих образовательную деятельность по
-                            имеющим государственную аккредитацию образовательным программам» (по состоянию на 10:49 «05» октября 2023 г.)
-                        </Link>
+                        <Link href="#">Показатели деятельности образовательной организации высшего образования, подлежащей самообследованию</Link>
+                    </li>
+                    <li className="flex items-center text-[18px] text-[#0F91D6] underline underline-offset-2">
+                        <Dot className="min-h-6 min-w-6" />
+                        <Link href="#">Порядок проведения самообследования образовательной организацией</Link>
                     </li>
                 </ul>
             </div>
@@ -52,7 +72,9 @@ const SvedenDocumentPage = () => {
                 <ul className="space-y-2">
                     <li className="flex items-center text-[18px] text-[#0F91D6] underline underline-offset-2">
                         <Dot className="min-h-6 min-w-6" />
-                        <Link href="#">Предписание об устранении выявленных нарушений №07-55-123/18-3 от 25.08.2017 г.</Link>
+                        <Link href="#" itemProp="prescriptionDocLink">
+                            Предписание об устранении выявленных нарушений №07-55-123/18-3 от 25.08.2017 г.
+                        </Link>
                     </li>
                     <li className="flex items-center text-[18px] text-[#0F91D6] underline underline-offset-2">
                         <Dot className="min-h-6 min-w-6" />
@@ -84,19 +106,84 @@ const SvedenDocumentPage = () => {
             </div>
 
             <div className="space-y-2 rounded-[10px] bg-white p-8">
-                <p className="text-[23px] font-semibold leading-[20px]">Отчет о результатах самообследования</p>
+                <p className="text-[23px] font-semibold leading-[20px]">
+                    Локальные нормативные акты образовательной организации по основным вопросам организации и осуществления образовательной
+                    деятельности
+                </p>
                 <ul className="space-y-2">
                     <li className="flex items-center text-[18px] text-[#0F91D6] underline underline-offset-2">
                         <Dot className="min-h-6 min-w-6" />
-                        <Link href="#">Отчет о результатах самообследования</Link>
+                        <Link href="#" itemProp="priemDocLink">
+                            Правила приема на обучение по образовательным программам высшего образования – программам бакалавриата, программам
+                            специалитета, программам магистратуры в ФГБОУ ВО Воронежский ГАУ на 2024/2025 учебный год
+                        </Link>
                     </li>
                     <li className="flex items-center text-[18px] text-[#0F91D6] underline underline-offset-2">
                         <Dot className="min-h-6 min-w-6" />
-                        <Link href="#">Показатели деятельности образовательной организации высшего образования, подлежащей самообследованию</Link>
+                        <Link href="#" itemProp="priemDocLink">
+                            Правила приема на 2024-2025 учебный год на обучение по образовательным программам высшего образования – программам
+                            подготовки научных и научно-педагогических кадров в аспирантуре
+                        </Link>
                     </li>
                     <li className="flex items-center text-[18px] text-[#0F91D6] underline underline-offset-2">
                         <Dot className="min-h-6 min-w-6" />
-                        <Link href="#">Порядок проведения самообследования образовательной организацией</Link>
+                        <Link href="#" itemProp="priemDocLink">
+                            Правила приема на 2023-2024 учебный год на обучение по образовательным программам среднего профессионального образования
+                        </Link>
+                    </li>
+                    <li className="flex items-center text-[18px] text-[#0F91D6] underline underline-offset-2">
+                        <Dot className="min-h-6 min-w-6" />
+                        <Link href="#" itemProp="priemDocLink">
+                            Особенности приема на обучение в федеральное государственное бюджетное образовательное учреждение высшего образования
+                            «Воронежский государственный аграрный университет имени императора Петра I» осуществляющее образовательную деятельность,
+                            по программам бакалавриата, программам специалитета, программам магистратуры и программам подготовки научно-педагогических
+                            кадров в аспирантуре, предусмотренные частями 7 и 8 статьи 5 Федерального закона от 17 февраля 2023 г. N° 19-Ф3 «Об
+                            особенностях правового регулирования отношений в сферах образования и науки в связи с принятием в Российскую Федерацию
+                            Донецкой Народной Республики, Луганской Народной Республики, Запорожской области, Херсонской области и образованием в
+                            составе Российской Федерации новых субъектов – Донецкой Народной Республики, Луганской Народной Республики, Запорожской
+                            области, Херсонской области и о внесении изменений в отдельные законодательные акты Российской Федерации»
+                        </Link>
+                    </li>
+                    <li className="flex items-center text-[18px] text-[#0F91D6] underline underline-offset-2">
+                        <Dot className="min-h-6 min-w-6" />
+                        <Link href="#" itemProp="priemDocLink">
+                            Особенности приема на обучение в федеральное государственное бюджетное образовательное учреждение высшего образования
+                            «Воронежский государственный аграрный университет имени императора Петра I» по образовательным программам высшего
+                            образования, имеющим государственную аккредитацию, программам подготовки научных и научно-педагогических кадров в
+                            аспирантуре в 2023 году
+                        </Link>
+                    </li>
+                    <li className="flex items-center text-[18px] text-[#0F91D6] underline underline-offset-2">
+                        <Dot className="min-h-6 min-w-6" />
+                        <Link href="#" itemProp="priemDocLink">
+                            Особенности приема на обучение по программам бакалавриата, программам специалитета лиц, обучавшихся в образовательных
+                            организациях, расположенных на приграничных территориях Российской Федерации
+                        </Link>
+                    </li>
+                    <li className="flex items-center text-[18px] text-[#0F91D6] underline underline-offset-2">
+                        <Dot className="min-h-6 min-w-6" />
+                        <Link href="#" itemProp="modeDocLink">
+                            Режим занятий обучающихся
+                        </Link>
+                    </li>
+                    <li className="flex items-center text-[18px] text-[#0F91D6] underline underline-offset-2">
+                        <Dot className="min-h-6 min-w-6" />
+                        <Link href="#" itemProp="tekKontrolDocLink">
+                            Формы, периодичность и порядок текущего контроля успеваемости и промежуточной аттестации обучающихся
+                        </Link>
+                    </li>
+                    <li className="flex items-center text-[18px] text-[#0F91D6] underline underline-offset-2">
+                        <Dot className="min-h-6 min-w-6" />
+                        <Link href="#" itemProp="perevodDocLink">
+                            Порядок и основания перевода, отчисления и восстановления обучающихся
+                        </Link>
+                    </li>
+                    <li className="flex items-center text-[18px] text-[#0F91D6] underline underline-offset-2">
+                        <Dot className="min-h-6 min-w-6" />
+                        <Link href="#" itemProp="vozDocLink">
+                            Порядок оформления возникновения, приостановления и прекращения отношений между образовательной организацией и
+                            обучающимися и (или) родителями (законными представителями) несовершеннолетних обучающихся
+                        </Link>
                     </li>
                 </ul>
             </div>
