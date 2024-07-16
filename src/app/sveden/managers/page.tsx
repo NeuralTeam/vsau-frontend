81e23245569e3fdaaaf83d1eeabe59291e4fceba
Osborn <dencat4785@gmail.com>
2024-07-17 02:07:07 +0300

    feat: сделана страница "Руководство"

--- src/app/sveden/managers/page.tsx
+++ src/app/sveden/managers/page.tsx
@@ -1,5 +1,99 @@
+import Image from "next/image";
+
 const SvedenManagersPage = () => {
-    return <main>SvedenManagersPage</main>;
+    return (
+        <main itemProp="copy" className="space-y-8 pr-[70px] pt-[50px]">
+            <h1>Руководство</h1>
+
+            <div className="grid grid-cols-2 grid-rows-3 gap-5 rounded-[10px] bg-white p-8">
+                {Array.from({ length: 6 }).map((_, i) => (
+                    <div key={i} className="flex max-w-[650px] items-center space-x-10 rounded-[10px] bg-[#0F91D6] p-10">
+                        <div>
+                            <Image src="/manifest_logo512_example.png" width={180} height={180} alt="#" />
+                        </div>
+                        <div itemProp={(i + 1) % 2 == 0 ? "rucovodstvo" : "rucovodstvoZam"} className="space-y-4 text-white">
+                            <div className="space-y-2">
+                                <p itemProp="fio" className="text-[25px] font-semibold leading-[25px]">
+                                    Агибалов Александр Владимирович
+                                </p>
+                                <p itemProp="post" className="text-[17px] font-light leading-[16px]">
+                                    Ректор Воронежского ГАУ
+                                </p>
+                            </div>
+                            <div className="space-y-0.5 text-[17px] font-light leading-[18px]">
+                                <p itemProp="telephone">253-86-31 / 101 каб.</p>
+                                <p itemProp="email">main@vsau.ru</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            <div itemProp="rucovodstvoFil" className="rounded-[10px] bg-white p-8">
+                {Array.from({ length: 2 }).map((_, i) => (
+                    <div key={i}>
+                        <p itemProp="nameFil" className="text-[23px] font-semibold leading-[20px]">
+                            Филиал №{i + 1}
+                        </p>
+
+                        <div className="grid grid-cols-2 gap-5">
+                            {Array.from({ length: 1 }).map((_, i) => (
+                                <div key={i} className="max-w-[650px] items-center p-10">
+                                    <div className="space-y-4">
+                                        <div className="space-y-2">
+                                            <p itemProp="fio" className="text-[25px] font-semibold leading-[25px] text-[#0F91D6]">
+                                                Агибалов Александр Владимирович
+                                            </p>
+                                            <p itemProp="post" className="text-[17px] font-light leading-[16px]">
+                                                Ректор Воронежского ГАУ
+                                            </p>
+                                        </div>
+                                        <div className="space-y-0.5 text-[17px] font-light leading-[20px]">
+                                            <p>Корпус главный, 101 каб.</p>
+                                            <p itemProp="telephone">253-86-31</p>
+                                            <p itemProp="email">main@vsau.ru</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            <div itemProp="rucovodstvoRep" className="rounded-[10px] bg-white p-8">
+                {Array.from({ length: 2 }).map((_, i) => (
+                    <div key={i}>
+                        <p itemProp="nameRep" className="text-[23px] font-semibold leading-[20px]">
+                            Представительство №{i + 1}
+                        </p>
+
+                        <div className="grid grid-cols-2 gap-5">
+                            {Array.from({ length: 1 }).map((_, i) => (
+                                <div key={i} className="max-w-[650px] items-center p-10">
+                                    <div className="space-y-4">
+                                        <div className="space-y-2">
+                                            <p itemProp="fio" className="text-[25px] font-semibold leading-[25px] text-[#0F91D6]">
+                                                Агибалов Александр Владимирович
+                                            </p>
+                                            <p itemProp="post" className="text-[17px] font-light leading-[16px]">
+                                                Ректор Воронежского ГАУ
+                                            </p>
+                                        </div>
+                                        <div className="space-y-0.5 text-[17px] font-light leading-[20px]">
+                                            <p>Корпус главный, 101 каб.</p>
+                                            <p itemProp="telephone">253-86-31</p>
+                                            <p itemProp="email">main@vsau.ru</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </main>
+    );
 };
 
 export default SvedenManagersPage;
