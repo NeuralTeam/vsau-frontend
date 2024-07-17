755f54f8732a20b2ad3613b113bba0225f903581
Osborn <dencat4785@gmail.com>
2024-07-17 22:18:51 +0300

    test: проверка компонента с алфавитным скроллом

--- src/app/sveden/employees/page.tsx
+++ src/app/sveden/employees/page.tsx
@@ -1,96 +1,40 @@
-"use client";
-
-import { cn } from "@/shared/libs/shadcn-utils";
-import { useState } from "react";
-import { InView } from "react-intersection-observer";
-import { TemplatePage } from "@/shared/ui/vsau/sveden";
-
-function getAlphabet() {
-    const startCharCode = "а".charCodeAt(0);
-    const endCharCode = "я".charCodeAt(0);
-
-    let russianAlphabet: string[] = [];
-
-    for (let i = startCharCode; i <= endCharCode; i++) {
-        russianAlphabet.push(String.fromCharCode(i));
-    }
-
-    return russianAlphabet;
-}
+import { TeachingStaffBlock, TemplatePage } from "@/shared/ui/vsau/sveden";
 
 const SvedenEmployeesPage = () => {
-    const letters = getAlphabet();
-
-    const [hash, setHash] = useState("а");
-    const [state, setState] = useState(false);
-
-    const setLetterState = (hash: string, hashAuto: boolean) => {
-        if (!hashAuto) {
-            const element = document.getElementById(hash);
-            element?.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
-        }
-        setHash(hash);
-    };
+    const a = [
+        { last_name: "фsd" },
+        { last_name: "яad" },
+        { last_name: "аsd" },
+        { last_name: "ыфаsd" },
+        { last_name: "урsd" },
+        { last_name: "ецуеsd" },
+        { last_name: "нцуе" },
+        { last_name: "гыфа" },
+        { last_name: "шсм" },
+        { last_name: "щпр" },
+        { last_name: "зцк" },
+        { last_name: "хйе" },
+        { last_name: "паИ" },
+        { last_name: "рцк" },
+        { last_name: "ое" },
+        { last_name: "луе" },
+        { last_name: "дтыун" },
+        { last_name: "жц" },
+        { last_name: "эцс" },
+        { last_name: "яци" },
+        { last_name: "чяр" },
+        { last_name: "саив" },
+        { last_name: "ми" },
+        { last_name: "ии" },
+        { last_name: "ти" },
+        { last_name: "би" },
+        { last_name: "юи" },
+        { last_name: "абв" }
+    ];
 
     return (
         <TemplatePage title="Педагогический состав">
-            <div className="space-y-8 rounded-[10px] bg-white py-8">
-                <p className="px-8 text-[23px] font-semibold leading-[20px]">
-                    Информация о персональном составе педагогических работников образовательной программы
-                </p>
-
-                <div className="flex h-svh justify-between space-x-8 overflow-hidden pr-8">
-                    <div className="h-full w-full space-y-4 overflow-auto py-1 pl-8 pr-4">
-                        {letters.map((char, i) => (
-                            <InView
-                                key={char}
-                                id={char}
-                                threshold={0}
-                                as="div"
-                                onChange={(inView, entry) => inView && setLetterState(entry?.target.id, true)}
-                            >
-                                <p className="text-[26px] font-bold text-[#0F91D6]">{char.toUpperCase()}</p>
-                                <div className="space-y-4">
-                                    {Array.from({ length: 14 }).map((_, i) => (
-                                        <div key={i} itemProp="teachingStaff" className="flex w-full flex-col rounded-[10px] py-4 pl-10 shadow">
-                                            <p itemProp="fio" onClick={() => setState(!state)} className="text-[18px]">
-                                                Авдеев Евгений Валентинович
-                                            </p>
-                                            <p itemProp="post" className="text-[13px]">
-                                                Доцент кафедры организации производства и предпринимательской деятельности в АПК
-                                            </p>
-
-                                            <div className={cn("max-h-0 overflow-hidden duration-300 ease-linear", state && "max-h-[700px]")}>
-                                                <p itemProp="teachingDiscipline">teachingDiscipline</p>
-                                                <p itemProp="teachingLevel">teachingLevel</p>
-                                                <p itemProp="employeeQualification">employeeQualification</p>
-                                                <p itemProp="degree">degree</p>
-                                                <p itemProp="academStat">academStat</p>
-                                                <p itemProp="profDevelopment">profDevelopment</p>
-                                                <p itemProp="genExperience">genExperience</p>
-                                                <p itemProp="specExperience">specExperience</p>
-                                                <p itemProp="teachingOp">teachingOp</p>
-                                            </div>
-                                        </div>
-                                    ))}
-                                </div>
-                            </InView>
-                        ))}
-                    </div>
-
-                    <div className="flex h-full flex-col justify-between rounded-[10px] bg-[#0F91D6] p-5">
-                        {letters.map((char, i) => (
-                            <button
-                                key={i}
-                                onClick={() => setLetterState(char, false)}
-                                className={cn("text-center text-[15px] font-bold text-white", hash == char && "opacity-50")}
-                            >
-                                {char.toUpperCase()}
-                            </button>
-                        ))}
-                    </div>
-                </div>
-            </div>
+            <TeachingStaffBlock staffList={a} />
         </TemplatePage>
     );
 };
--- src/shared/ui/vsau/sveden/teaching-staff-block.tsx
+++ src/shared/ui/vsau/sveden/teaching-staff-block.tsx
@@ -1,3 +1,125 @@
-export const TeachingStaffBlock = () => {
-    return <div></div>;
+"use client";
+
+import { InView } from "react-intersection-observer";
+import { cn } from "@/shared/libs/shadcn-utils";
+import { useState } from "react";
+
+interface IDiscipline {}
+
+interface IProfDevelopment {}
+
+interface IEduProgram {}
+
+interface ITeachingStaff {
+    id: number;
+    first_name: string;
+    last_name: string;
+    post: string;
+    disciplines: IDiscipline[];
+    level: string;
+    qualification: string;
+    degree?: string;
+    academStat?: string;
+    profDevelopments?: IProfDevelopment[];
+    totalWorkExperience: number;
+    specWorkExperience: number;
+    eduPrograms: IEduProgram[];
+}
+
+interface ITeachingStaffBlock {
+    staffList: ITeachingStaff[];
+}
+
+export const TeachingStaffBlock = ({ staffList }: ITeachingStaffBlock) => {
+    const sortedList = Object.fromEntries(
+        Object.entries(
+            staffList.reduce(
+                (acc, { last_name }) => {
+                    // Получаем первую букву или пустую строку
+                    const firstLetter = last_name[0] || "";
+                    // Добавляем объект в соответствующий массив в аккумуляторе
+                    acc[firstLetter] = [...(acc[firstLetter] || []), { last_name }];
+                    return acc;
+                },
+                {} as Record<string, { last_name: string }[]>
+            )
+        ).sort(([keyA], [keyB]) => keyA.localeCompare(keyB))
+    );
+
+    const [hash, setHash] = useState("а");
+    const [state, setState] = useState(false);
+
+    const setLetterState = (hash: string, hashAuto: boolean) => {
+        if (!hashAuto) {
+            const element = document.getElementById(hash);
+            element?.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+        }
+        setHash(hash);
+    };
+
+    return (
+        <div className="space-y-8 rounded-[10px] bg-white py-8">
+            <p className="px-8 text-[23px] font-semibold leading-[20px]">
+                Информация о персональном составе педагогических работников образовательной программы
+            </p>
+
+            <div className="flex h-svh justify-between space-x-8 overflow-hidden pr-8">
+                <div className="h-full w-full space-y-4 overflow-auto py-1 pl-8 pr-4">
+                    {Object.entries(sortedList).map(([char, valueList]) => (
+                        <InView
+                            key={char}
+                            id={char}
+                            threshold={0.1}
+                            rootMargin="0% 0% -90% 0%"
+                            as="div"
+                            onChange={(inView) => inView && setLetterState(char, true)}
+                        >
+                            <p className="text-[26px] font-bold text-[#0F91D6]">{char.toUpperCase()}</p>
+                            <ul className="relative space-y-4">
+                                {valueList.map((staff) => (
+                                    <li
+                                        key={staff.last_name}
+                                        itemProp="teachingStaff"
+                                        className="flex w-full flex-col rounded-[10px] py-4 pl-10 shadow"
+                                    >
+                                        <p itemProp="fio" onClick={() => setState(!state)} className="text-[18px]">
+                                            {staff.last_name}
+                                            {/*{staff.first_name}*/}
+                                        </p>
+                                        <p itemProp="post" className="text-[13px]">
+                                            {/*{staff.post}*/}
+                                        </p>
+
+                                        <div className={cn("max-h-0 overflow-hidden duration-300 ease-linear", state && "max-h-[700px]")}>
+                                            <p itemProp="teachingDiscipline">teachingDiscipline</p>
+                                            {/*<p itemProp="teachingLevel">{staff.level}</p>*/}
+                                            <p itemProp="employeeQualification">employeeQualification</p>
+                                            {/*<p itemProp="degree">{staff.degree}</p>*/}
+                                            {/*<p itemProp="academStat">{staff.academStat}</p>*/}
+                                            <p itemProp="profDevelopment">profDevelopment</p>
+                                            {/*<p itemProp="genExperience">{staff.totalWorkExperience}</p>*/}
+                                            {/*<p itemProp="specExperience">{staff.specWorkExperience}</p>*/}
+                                            <p itemProp="teachingOp">teachingOp</p>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        </InView>
+                    ))}
+                </div>
+
+                <div className="sticky top-0 flex h-full flex-col rounded-[10px] bg-[#0F91D6] p-5">
+                    {Object.keys(sortedList).map((char) => (
+                        <button
+                            key={char}
+                            onClick={() => setLetterState(char, false)}
+                            className={cn("text-center text-[15px] font-bold text-white", hash == char && "opacity-50")}
+                        >
+                            {char.toUpperCase()}
+                        </button>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 };
