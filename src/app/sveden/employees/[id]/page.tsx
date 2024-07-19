b86ba9790cb1545076dc633a31181b82d567ac0a
Osborn <dencat4785@gmail.com>
2024-07-19 16:05:56 +0300

    feat: начата страница "выбор преподавателя"

--- src/app/sveden/employees/[id]/page.tsx
+++ src/app/sveden/employees/[id]/page.tsx
@@ -1,10 +1,32 @@
 import { ShareBlock } from "@/shared/ui/vsau/share-block";
+import { UserRound } from "lucide-react";
 
 const SvedenEmployerPage = ({ params }: { params: { id: string } }) => {
     return (
-        <div className="space-y-8 pr-[70px] pt-[50px]">
-            <div></div>
-            <div className="h-20 w-full rounded-[10px] bg-white">A</div>
+        <div className="space-y-4 pr-[70px] pt-[50px]">
+            <div className="flex space-x-4">
+                <div className="rounded-[10px] bg-white p-8"></div>
+                <div className="flex items-center justify-center space-x-8 rounded-[10px] bg-white p-8">
+                    <div className="flex min-h-32 min-w-32 justify-center overflow-hidden rounded-full bg-[#ECEBEB]">
+                        <UserRound size={112} strokeWidth={1} fill="#FFFFFF" color="#FFFFFF" className="relative top-8" />
+                    </div>
+
+                    <p className="text-[30px] font-bold text-[#0F91D6]">Авдеев Евгений Валентинович</p>
+                </div>
+                <div></div>
+            </div>
+            <div className="flex h-20 w-full flex-col justify-center rounded-[10px] bg-white p-8">
+                <p className="text-[14px] font-light">Занимаемая должность (должности)</p>
+                <p className="text-[16px] font-medium">Доцент кафедры организации производства и предпринимательской деятельности в АПК</p>
+            </div>
+            <div className="flex h-20 w-full flex-col justify-center rounded-[10px] bg-white p-8">
+                <p className="text-[14px] font-light">Перечень преподаваемых дисциплин</p>
+                <p className="text-[16px] font-medium">Доцент кафедры организации производства и предпринимательской деятельности в АПК</p>
+            </div>
+            <div className="flex h-20 w-full flex-col justify-center rounded-[10px] bg-white p-8">
+                <p className="text-[14px] font-light">Уровень образования</p>
+                <p className="text-[16px] font-medium">Доцент кафедры организации производства и предпринимательской деятельности в АПК</p>
+            </div>
             <ShareBlock />
         </div>
     );
