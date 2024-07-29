04db98906ef1e85d9f7d8d9cc3a106bbdccaecc9
Osborn <dencat4785@gmail.com>
2024-07-29 22:43:55 +0300

    feat: добавлен выпадающий выбор раздела

--- src/widgets/news/news-filter.tsx
+++ src/widgets/news/news-filter.tsx
@@ -2,7 +2,9 @@
 
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/shared/ui/shadcn/accordion";
+import { NavCollapseArrow } from "@/shared/images/icons/nav-collapse-arrow";
+import { useEffect, useState } from "react";
 
 export interface ITopic {
     id: number;
@@ -13,17 +15,36 @@ const SectionFilter = ({ topics }: { topics: ITopic[] }) => {
     const query = useSearchParams();
     const currentTopic = query.get("topic") ?? 0;
 
+    const [topic, setTopic] = useState("Всё");
+
+    useEffect(() => {
+        const topicArr = topics.filter((v) => v.id == currentTopic);
+        if (topicArr.length === 0) {
+            setTopic("Всё");
+            return;
+        }
+        setTopic(topicArr[0].title);
+    }, [currentTopic]);
+
     return (
-        <div className="flex w-[300px] flex-col rounded-[5px] bg-white p-8">
-            <Link href="/news" className={currentTopic == 0 ? "font-semibold" : ""}>
-                Всё
-            </Link>
-            {topics.map((data) => (
-                <Link key={data.id} href={`/news?topic=${data.id}`} className={currentTopic == data.id ? "font-semibold" : ""}>
-                    {data.title.charAt(0).toUpperCase() + data.title.slice(1)}
-                </Link>
-            ))}
-        </div>
+        <Accordion type="single" collapsible>
+            <AccordionItem value="item-1" className="space-y-4 rounded-[10px] bg-white px-6 py-4">
+                <AccordionTrigger className="flex justify-between px-4 py-2">
+                    {topic}
+                    <NavCollapseArrow width={11} height={10} fillColor="#000000" className="-rotate-90 duration-300" />
+                </AccordionTrigger>
+                <AccordionContent className="flex flex-col px-4 py-2">
+                    <Link href="/news" className={currentTopic == 0 ? "font-semibold" : ""}>
+                        Всё
+                    </Link>
+                    {topics.map((data) => (
+                        <Link key={data.id} href={`/news?topic=${data.id}`} className={currentTopic == data.id ? "font-semibold" : ""}>
+                            {data.title.charAt(0).toUpperCase() + data.title.slice(1)}
+                        </Link>
+                    ))}
+                </AccordionContent>
+            </AccordionItem>
+        </Accordion>
     );
 };
 
