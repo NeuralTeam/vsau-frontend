563f88afccb393ecbe5145f694f6bfb5742fdf9f
Osborn <dencat4785@gmail.com>
2024-07-29 22:07:18 +0300

    feat: добавлены кнопки для карусели постов на главной странице

--- src/shared/ui/shadcn/carousel.tsx
+++ src/shared/ui/shadcn/carousel.tsx
@@ -164,22 +164,10 @@ const CarouselPrevious = React.forwardRef<HTMLButtonElement, React.ComponentProp
         const { orientation, scrollPrev, canScrollPrev } = useCarousel();
 
         return (
-            <Button
-                ref={ref}
-                variant={variant}
-                size={size}
-                className={cn(
-                    "absolute h-8 w-8 rounded-full",
-                    orientation === "horizontal" ? "-left-12 top-1/2 -translate-y-1/2" : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
-                    className
-                )}
-                disabled={!canScrollPrev}
-                onClick={scrollPrev}
-                {...props}
-            >
-                <ArrowLeft className="h-4 w-4" />
+            <button ref={ref} className={className} disabled={!canScrollPrev} onClick={scrollPrev} {...props}>
+                <ArrowLeft color="#0F91D6" className="h-6 w-6" />
                 <span className="sr-only">Previous slide</span>
-            </Button>
+            </button>
         );
     }
 );
@@ -190,22 +178,10 @@ const CarouselNext = React.forwardRef<HTMLButtonElement, React.ComponentProps<ty
         const { orientation, scrollNext, canScrollNext } = useCarousel();
 
         return (
-            <Button
-                ref={ref}
-                variant={variant}
-                size={size}
-                className={cn(
-                    "absolute h-8 w-8 rounded-full",
-                    orientation === "horizontal" ? "-right-12 top-1/2 -translate-y-1/2" : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
-                    className
-                )}
-                disabled={!canScrollNext}
-                onClick={scrollNext}
-                {...props}
-            >
-                <ArrowRight className="h-4 w-4" />
+            <button ref={ref} className={className} disabled={!canScrollNext} onClick={scrollNext} {...props}>
+                <ArrowRight color="#0F91D6" className="h-6 w-6" />
                 <span className="sr-only">Next slide</span>
-            </Button>
+            </button>
         );
     }
 );
